2d8fde8872d01eb3f3fda5d42e3a4efc0e11b772
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-10-15 17:46:52 -0500

    add view for edit and delete note

--- index.js
+++ index.js
@@ -54,6 +54,49 @@ app.get('/notes/:id', async (req, res) => {
     })
 })
 
+app.get('/notes/:id/edit', async (req, res, next) => {
+    try {
+        const notes = await Note.find()
+        const note = await Note.findById(req.params.id)
+
+        res.render('edit', {
+            notes: notes,
+            currentNote: note
+        })
+    } catch(err) {
+        return next(err)
+    }
+})
+
+app.patch('/notes/:id', async (req, res, next) => {
+    const id = req.params.id
+    const note = await note.findById(id)
+
+    note.title = req.body.title
+    note.body = req.body.body
+
+    try {
+        await note.save({})
+    } catch(err) {
+        return next(err)
+    }
+
+    res.status(204)
+    res.send({})
+})
+
+app.delete('/notes/:id', async (req, res, next) => {
+    try {
+        await Note.deleteOne({
+        _id: req.params.id
+        })
+    }catch(err) {
+        return next(err)
+    }
+    
+    res.status(204)
+    res.send({})
+})
 // app.use((err, req, res, next) => {
 //     res.status(500)
 //     res.send('<h1>Error inesperado</h1><p>${err.message}</p>')
